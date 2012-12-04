45c416e8a4e3ba6563f4e2e593c7b8870f2a29a1
Richard Bormann <Richard.Bormann@ipa.fraunhofer.de>
2012-12-04 11:43:55 +0100

    modifications for robot usage

--- cob_image_flip/ros/src/kinect_image_flip.cpp
+++ cob_image_flip/ros/src/kinect_image_flip.cpp
@@ -126,10 +126,10 @@ public:
 		//sync_pointcloud_ = 0;
 		transform_listener_ = 0;
 
-//		it_ = new image_transport::ImageTransport(node_handle_);
-//		color_camera_image_sub_.subscribe(*it_, "colorimage_in", 1);
-//		color_camera_image_sub_.registerCallback(boost::bind(&CobKinectImageFlip::imageCallback, this, _1));
-//		color_camera_image_pub_ = it_->advertise("colorimage_out", 1);
+		it_ = new image_transport::ImageTransport(node_handle_);
+		color_camera_image_sub_.subscribe(*it_, "colorimage_in", 1);
+		color_camera_image_sub_.registerCallback(boost::bind(&CobKinectImageFlip::imageCallback, this, _1));
+		color_camera_image_pub_ = it_->advertise("colorimage_out", 1);
 
 		point_cloud_sub_ = node_handle_.subscribe<sensor_msgs::PointCloud2>("pointcloud_in", 1, &CobKinectImageFlip::inputCallback, this);
 		point_cloud_pub_ = node_handle_.advertise<sensor_msgs::PointCloud2>("pointcloud_out", 1);
@@ -214,24 +214,22 @@ public:
 			pcl::fromROSMsg(*point_cloud_msg, point_cloud_src);
 
 			pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_turned(new pcl::PointCloud<pcl::PointXYZRGB>);
-			point_cloud_turned->resize(point_cloud_src.height*point_cloud_src.width);
-			int point_cloud_turned_counter = 0;
-			for (int v = (int)point_cloud_src.height - 1; v >= 0; v--)
-			{
-				for (int u = (int)point_cloud_src.width - 1; u >= 0; u--)
-				{
-					point_cloud_turned->at(point_cloud_turned_counter) = point_cloud_src(u, v);
-					point_cloud_turned_counter++;
-				}
-			}
-
-			// publish turned data
 			point_cloud_turned->header = point_cloud_msg->header;
 			point_cloud_turned->height = point_cloud_msg->height;
 			point_cloud_turned->width = point_cloud_msg->width;
 			//point_cloud_turned->sensor_orientation_ = point_cloud_msg->sensor_orientation_;
 			//point_cloud_turned->sensor_origin_ = point_cloud_msg->sensor_origin_;
-			point_cloud_turned->is_dense = point_cloud_msg->is_dense;
+			point_cloud_turned->is_dense = true;	//point_cloud_msg->is_dense;
+			point_cloud_turned->resize(point_cloud_src.height*point_cloud_src.width);
+			for (int v = (int)point_cloud_src.height - 1; v >= 0; v--)
+			{
+				for (int u = (int)point_cloud_src.width - 1; u >= 0; u--)
+				{
+					(*point_cloud_turned)(point_cloud_src.width-1 - u, point_cloud_src.height-1 - v) = point_cloud_src(u, v);
+				}
+			}
+
+			// publish turned data
 			sensor_msgs::PointCloud2::Ptr point_cloud_turned_msg(new sensor_msgs::PointCloud2);
 			pcl::toROSMsg(*point_cloud_turned, *point_cloud_turned_msg);
 			//point_cloud_turned_msg->header.stamp = ros::Time::now();
@@ -302,6 +300,12 @@ public:
 					dst -= 6;
 				}
 			}
+	        cv_bridge::CvImage cv_ptr;
+	        cv_ptr.image = color_image_turned;
+	        cv_ptr.encoding = "bgr8";
+	        sensor_msgs::Image::Ptr color_image_turned_msg = cv_ptr.toImageMsg();
+	        color_image_turned_msg->header = color_image_msg->header;
+	        color_camera_image_pub_.publish(color_image_turned_msg);
 		}
 	}
 };
